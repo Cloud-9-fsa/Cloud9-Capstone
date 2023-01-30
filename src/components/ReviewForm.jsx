05839f45b387449f159b4ee27bb66d3fc78396d1
Mohamad Ayyad <hamoodaayyad@gmail.com>
2023-01-30 12:40:42 -0600

    added total

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useAuth } from "../context/UseAuth";
+import { createReview } from "../apiCalls/createReview";
+
+export const ReviewForm = ({ listingId }) => {
+  const { user, token } = useAuth();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState(5);
+
+  const handleSubmit = async (e) => {
+    if (token) {
+      e.preventDefault();
+
+      await createReview(
+        listingId,
+        user.id,
+        user.firstname,
+        user.lastname,
+        title,
+        description,
+        rating,
+        token
+      );
+      setTitle("");
+      setRating("");
+      setDescription("");
+    } else {
+      window.alert("Please Login or Register");
+    }
+  };
+
+  return (
+    <div>
+      <div className="newReviewForm">
+        <h4>Review</h4>
+        <form className="reviewForm" onSubmit={(e) => handleSubmit(e)}>
+          <input
+            value={title}
+            type="text"
+            placeholder="title"
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          <input
+            value={rating}
+            type="number"
+            placeholder="rating"
+            onChange={(e) => setRating(e.target.value)}
+          ></input>
+          <input
+            value={description}
+            type="text"
+            placeholder="description"
+            onChange={(e) => setDescription(e.target.value)}
+          ></input>
+          <br></br>
+          <button className="reviewSubmit" type="submit">
+            Submit Review
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
