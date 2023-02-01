631c25533c969aba52c0e5bd8519f58ca7abff22
Christopher Pettypiece <chasepettypiece@gmail.com>
2023-02-01 14:39:31 -0600

    cartCSS

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -19,46 +19,66 @@ export const Cart = () => {
     const CartListings = order.listings.map((listing) => {
       return (
         <div className="cartForm" key={listing.id}>
-          <h1>Name:{listing.name}</h1>
-          <h1>Price:{listing.price}</h1>
+          <div className="upperForm">
+            <div className="text1">
+              <h1>{listing.name}</h1>
+              <h1>Price: ${listing.price}.00</h1>
+              <div className="descript">
+                <h1>{listing.description}</h1>
+              </div>
+            </div>
+            <img className="theimage" src={listing.image} />
 
-          <label>Quantity:</label>
-          <input
-            type="number"
-            id="quantity"
-            name="quantity"
-            placeholder={listing.quantity}
-            onChange={async (e) => {
-              console.log();
-              await editOrder(order.id, listing.orderListingId, e.target.value);
-              const oldOrder = await getOrder(token);
-              setOrder(oldOrder[0]);
-            }}
-            min="1"
-          />
-          <button
-            onClick={async () => {
-              console.log("hi");
-              await deleteListing(listing.id, order.id);
-              const oldOrder = await getOrder(token);
-              setOrder(oldOrder[0]);
-            }}
-          >
-            Remove From Cart
-          </button>
+            <div className="quantityBar">
+              <label>Quantity:</label>
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                placeholder={listing.quantity}
+                onChange={async (e) => {
+                  console.log();
+                  await editOrder(
+                    order.id,
+                    listing.orderListingId,
+                    e.target.value
+                  );
+                  const oldOrder = await getOrder(token);
+                  setOrder(oldOrder[0]);
+                }}
+                min="1"
+              />
+              <br></br>
+              {/* <div className="buttonCart"> */}
+              <button
+                className="button"
+                onClick={async () => {
+                  console.log("hi");
+                  await deleteListing(listing.id, order.id);
+                  const oldOrder = await getOrder(token);
+                  setOrder(oldOrder[0]);
+                }}
+              >
+                Remove From Cart
+              </button>
+              <br></br>
+              {/* </div> */}
+            </div>
+          </div>
         </div>
       );
     });
 
     return (
-      <div>
+      <div className="mainDiv">
         <h1>
           You have {order.listings.length} items in your cart , {user.firstname}
           !
         </h1>
         {CartListings}
-        <h1>Your total is: {order.total}</h1>
+        <h1>Your total is: ${order.total}.00</h1>
         <button
+          className="button"
           onClick={() => {
             navigate("/cart/checkout");
           }}
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,56 +7,71 @@ export const Shop = () => {
     <div className="main">
       <div className="CampaignImg">
         <h1>Welcome To Cloud9</h1>
-        <img
-          className="picmain"
-          src="https://images.unsplash.com/flagged/photo-1553802922-28e2f719977d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        ></img>
+        <div className="picmain">
+          <img src="https://images.unsplash.com/flagged/photo-1553802922-28e2f719977d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"></img>
+        </div>
       </div>
 
       <div className="LinkHeader">
-        <h2 className="textit">Pet Pillows</h2>
-        <Link to="/shop/pet">
-          <img
-            className="picshop"
-            src="https://cdn.shopify.com/s/files/1/0616/1331/0131/products/0c8c4e6eddb70f14e41f7aa38dd05400_1080x.jpg?v=1666880239"
-          ></img>
-        </Link>
+        <div className="overHere">
+          <div className="textit1">
+            <h2>Pet Pillows</h2>
+          </div>
+          <Link to="/shop/pet">
+            <img
+              className="picshop1"
+              src="https://cdn.shopify.com/s/files/1/0616/1331/0131/products/0c8c4e6eddb70f14e41f7aa38dd05400_1080x.jpg?v=1666880239"
+            ></img>
+          </Link>
+        </div>
 
+        <div className="overHere">
+          <div className="textit2">
+            <h2>Sleep</h2>
+          </div>
+          <Link to="/shop/sleep">
+            <img
+              className="picshop2"
+              src="https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202305/0011/laurel-down-duvet-and-pillow-insert-o.jpg"
+            ></img>
+          </Link>
+        </div>
 
-        <h2 className="textit">Sleep</h2>
-        <Link to="/shop/sleep">
-          <img
-            className="picshop"
-            src="https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202305/0011/laurel-down-duvet-and-pillow-insert-o.jpg"
-          ></img>
-        </Link>
+        <div className="overHere">
+          <div className="textit3">
+            <h2>Decorative</h2>
+          </div>
+          <Link to="/shop/decorative">
+            <img
+              className="picshop3"
+              src="https://cb.scene7.com/is/image/Crate/CresteCrdryIvoryPillowsFSSF22/$web_pdp_main_carousel_high$/220818095037/creste-ivory-corduroy-throw-pillows-by-athena-calderone.jpg"
+            ></img>
+          </Link>
+        </div>
 
+        <div className="overHere">
+          <div className="textit4">
+            <h2>Outdoor</h2>
+          </div>
+          <Link to="/shop/outdoor">
+            <img
+              className="picshop4"
+              src="https://images.unsplash.com/photo-1572830061526-bbd2c00f516c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            ></img>
+          </Link>
+        </div>
 
-        <h2 className="textit">Decorative</h2>
-        <Link to="/shop/decorative">
-          <img
-            className="picshop"
-            src="https://cb.scene7.com/is/image/Crate/CresteCrdryIvoryPillowsFSSF22/$web_pdp_main_carousel_high$/220818095037/creste-ivory-corduroy-throw-pillows-by-athena-calderone.jpg"
-          ></img>
-        </Link>
-
-
-        <h2 className="textit">Outdoor</h2>
-        <Link to="/shop/outdoor">
-          <img
-            className="picshop"
-            src="https://images.unsplash.com/photo-1572830061526-bbd2c00f516c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-          ></img>
-        </Link>
-
-        <h2 className="textit">Accessories</h2>
-        <Link to="/shop/accessories">
-          <img
-            className="picshop"
-            src="https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202242/0046/chambers-italian-600-thread-count-percale-duvet-cover-sham-o.jpg"
-          ></img>
-        </Link>
-
+        <div className="overHere">
+          <div className="textit5">
+            <h2>Accessories</h2>
+          </div>
+          <Link to="/shop/accessories">
+            <img
+              className="picshop5"
+              src="https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202242/0046/chambers-italian-600-thread-count-percale-duvet-cover-sham-o.jpg"
+            ></img>
+          </Link>
+        </div>
       </div>
     </div>
   );
