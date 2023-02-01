b3bf8dcc38c973888cbbb0362c1289757ed7526c
Mohamad Ayyad <hamoodaayyad@gmail.com>
2023-02-01 15:04:53 -0600

    fixed cart css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -16,32 +16,6 @@ export const Cart = () => {
     await deleteListingFromOrder(listingId, orderId);
   };
 
-  console.log(order);
-  if (order && order.listings && order.listings.length) {
-    const CartListings = order.listings.map((listing) => {
-      return (
-        <div className="cartForm" key={listing.id}>
-          <div className="upperForm">
-            <div className="text1">
-              <h1>{listing.name}</h1>
-              <h1>Price: ${listing.price}.00</h1>
-              <div className="descript">
-                <h1>{listing.description}</h1>
-              </div>
-            </div>
-            <img className="theimage" src={listing.image} />
-
-            <div className="quantityBar">
-              <label>Quantity:</label>
-              <input
-                type="number"
-                id="quantity"
-                name="quantity"
-                placeholder={listing.quantity}
-                onChange={async (e) => {
-                  console.log();
-
-
   useEffect(() => {
     const orders = async () => {
       if (token) {
@@ -62,81 +36,58 @@ export const Cart = () => {
       const CartListings = order.listings.map((listing) => {
         return (
           <div className="cartForm" key={listing.id}>
-           <div className="upperForm">
-            <div className="text1">
-            <h1>Name:{listing.name}</h1>
-            <h1>Price:{listing.price}</h1>
-            <div className="descript">
-             <h1>{listing.description}</h1>
+            <div className="upperForm">
+              <div className="text1">
+                <h1>Name:{listing.name}</h1>
+                <h1>Price:{listing.price}</h1>
+
+                <div className="descript">
+                  <h1>{listing.description}</h1>
+                </div>
               </div>
-            </div>
-             <img className="theimage" src={listing.image} />
+              <img className="theimage" src={listing.image} />
+              <div className="quantityBar">
+                <label>Quantity:</label>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="quantity"
+                  placeholder={listing.quantity}
+                  onChange={async (e) => {
+                    if (e.target.value !== "") {
+                      await editOrder(
+                        order.id,
+                        listing.orderListingId,
+                        e.target.value
+                      );
+                      const oldOrder = await getOrder(token);
+                      setOrder(oldOrder[0]);
+                    }
+                  }}
+                  min="1"
+                />
 
- <div className="quantityBar">
-            <label>Quantity:</label>
-            
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              placeholder={listing.quantity}
-              onChange={async (e) => {
-                if (e.target.value !== "") {
-
-                  await editOrder(
-                    order.id,
-                    listing.orderListingId,
-                    e.target.value
-                  );
-                  const oldOrder = await getOrder(token);
-                  setOrder(oldOrder[0]);
-
-                }}
-                min="1"
-              />
-              <br></br>
-            
-              <button
-                className="button"
-                onClick={async () => {
-                  console.log("hi");
-                  await deleteListing(listing.id, order.id);
-                  const oldOrder = await getOrder(token);
-                  setOrder(oldOrder[0]);
-                }}
-              >
-                Remove From Cart
-              </button>
-              <br></br>
-          
-            </div>
-          </div>
-
-                }
-              }}
-              min="1"
-            />
-            <br></br>
-            <h1>Item Total: {listing.price * listing.quantity}</h1>
-            <button
-              onClick={async () => {
-                console.log("hi");
-                await deleteListing(listing.id, order.id);
-                const oldOrder = await getOrder(token);
-                setOrder(oldOrder[0]);
-              }}
-            >
-              Remove From Cart
-            </button>
-             <br></br>
-          </div>
+                <h1>Item Total: {listing.price * listing.quantity}</h1>
+                <button
+                  className="button"
+                  onClick={async () => {
+                    console.log("hi");
+                    await deleteListing(listing.id, order.id);
+                    const oldOrder = await getOrder(token);
+                    setOrder(oldOrder[0]);
+                  }}
+                >
+                  Remove From Cart
+                </button>
+                <br></br>
+              </div>
             </div>
           </div>
         );
       });
 
       return (
-        <div>
+        <div className="mainDiv">
           <h1>
             You have {order.listings.length} items in your cart ,{" "}
             {user.firstname}!
@@ -144,13 +95,13 @@ export const Cart = () => {
           {CartListings}
           <h1>Your total is: {order.total}</h1>
           <button
+            className="button"
             onClick={() => {
               navigate("/cart/checkout");
             }}
           >
             Checkout Here!
           </button>
-
         </div>
       );
     } else
@@ -165,56 +116,66 @@ export const Cart = () => {
       const NotLoggedInCartListings = order[0].listings.map((listing) => {
         return (
           <div className="cartForm" key={listing.id}>
-            <h1>Name:{listing.name}</h1>
-            <h1>Price:{listing.price}</h1>
-            <h1>Item Total: {listing.price * listing.quantity}</h1>
+            <div className="upperForm">
+              <div className="text1">
+                <h1>Name:{listing.name}</h1>
+                <h1>Price:{listing.price}</h1>
+                <div className="descript">
+                  <h1> {listing.description}</h1>
+                </div>
+              </div>
+              <img className="theimage" src={listing.image} />
+              <h1>Item Total: {listing.price * listing.quantity}</h1>
+              <div className="quantityBar">
+                <label>Quantity:</label>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="quantity"
+                  placeholder={listing.quantity}
+                  onChange={async (e) => {
+                    if (e.target.value !== "") {
+                      await editOrder(
+                        order[0].id,
+                        listing.orderListingId,
+                        e.target.value
+                      );
+                      const oldOrder = await getOrderById(
+                        Number(localStorage.getItem("orderId"))
+                      );
+                      setOrder(oldOrder);
+                    }
+                  }}
+                  min="1"
+                />
 
-            <label>Quantity:</label>
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              placeholder={listing.quantity}
-              onChange={async (e) => {
-                await editOrder(
-                  order[0].id,
-                  listing.orderListingId,
-                  e.target.value
-                );
-                const oldOrder = await getOrderById(
-                  Number(localStorage.getItem("orderId"))
-                );
-                setOrder(oldOrder);
-              }}
-              min="1"
-            />
-
-
-    return (
-      <div className="mainDiv">
-
-            <button
-              onClick={async () => {
-                console.log("hi");
-                await deleteListing(listing.id, order[0].id);
-                const oldOrder = await getOrderById(
-                  Number(localStorage.getItem("orderId"))
-                );
-                setOrder(oldOrder);
-              }}
-            >
-              Remove From Cart
-            </button>
+                <button
+                  className="button"
+                  onClick={async () => {
+                    console.log("hi");
+                    await deleteListing(listing.id, order[0].id);
+                    const oldOrder = await getOrderById(
+                      Number(localStorage.getItem("orderId"))
+                    );
+                    setOrder(oldOrder);
+                  }}
+                >
+                  Remove From Cart
+                </button>
+                <br></br>
+              </div>
+            </div>
           </div>
         );
       });
 
       return (
-        <div>
+        <div className="mainDiv">
           <h1>You have {order[0].listings.length} items in your cart !</h1>
           {NotLoggedInCartListings}
-          <h1>Your total is: {order.total}</h1>
+          <h1>Your total is: {order[0].total}</h1>
           <button
+            className="button"
             onClick={() => {
               navigate("/cart/checkout");
             }}
@@ -225,33 +186,10 @@ export const Cart = () => {
       );
     } else
       return (
-
         <h1>
           Seems like your Cart is empty. Go add some of our wonderful Cinder
           Blocks® to it!
         </h1>
-
-        {CartListings}
-        <h1>Your total is: ${order.total}.00</h1>
-        <button
-          className="button"
-          onClick={() => {
-            navigate("/cart/checkout");
-          }}
-        >
-          Checkout Here!
-        </button>
-      </div>
-    );
-  } else
-    return (
-      <h1>
-        Seems like your Cart is empty. Go add some of our wonderful Cinder
-        Blocks® to it!
-      </h1>
-    );
-
       );
   }
-
 };
