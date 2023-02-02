e6658c57d76975e7b3e8e067e0e77050dbb6c9ae
Rahmat Bakhshi <swishtransportation@gmail.com>
2023-02-02 12:00:06 -0500

    additionalCSS

--- src/components/PillowTalk.jsx
+++ src/components/PillowTalk.jsx
@@ -17,33 +17,33 @@ const PillowTalks = () => {
     </li3>
     </div>
     <div className="Visco">
-    <img src="https://www.mattressclarity.com/wp-content/uploads/2017/07/KapokFiberImage.jpg" alt="pillow:>"/>
+    <img src="https://d12mivgeuoigbq.cloudfront.net/magento-media/catalog/product/a/1/a172d88d-237d-4c0b-ace7-eb88f02ce295.jpg?w=640" alt="pillow:>" className="pillowIMG"/>
 
         <li5>
-            Visco-elastic memory foam
+            Visco-elastic foam
         </li5>
         <li6>Higher density visco-elastic foam reacts to body heat which allows it to form itself to the shape of a warm body within a few minutes. Lower density visco-elastic foam is pressure-sensitive and will form more quickly to the shape of the body. One of the advantages of visco-elastic pillows is that by forming your shape, they can help achieve a neutral spinal alignment quickly and comfortably. Visco-elastic is often referred to as ‘memory foam’.
 
         </li6>
     </div>
     <div className="Polyester">
-        <img src="https://www.mattressclarity.com/wp-content/uploads/2017/07/KapokFiberImage.jpg" alt="pillow:>"/>
+        <img src="https://foamonline.com/wp-content/uploads/2020/08/Polyester-Pillow.jpg" alt="pillow:>" className="pillowIMG"/>
         <li5>Polyster</li5>
         <li6>These pillows are filled with polyester particles and filled to various depths and densities that assist in altering the feel of the pillow. This method used to be the dominant filling; however, As of today foams and latex have become much more popular, mostly due to being more resilient and providing better support.</li6>
         </div>
 
     <div className="Down">
-        <img src="https://www.mattressclarity.com/wp-content/uploads/2017/07/KapokFiberImage.jpg" alt="pillow:>"/>
+        <img src="https://cdn.shopify.com/s/files/1/0024/3233/5919/products/img-z-3_2579e212-9fed-4ec8-a347-245913987547_2400x2400.jpg?v=1672024229" alt="pillow:>" className="pillowIMG"/>
         <li5>Down</li5>
         <li6>Feathers have spines and look two-dimensional. They are the rough plumage found on the exterior of fowl, and can range in various sizes. Down is found on the belly of waterfowl and is the light fluffy coating clustered beneath their feathers that protects them from the elements. Feather and down pillows may be very comfortable and warm, but they offer very little support for the neck and spine.</li6>
     </div>
     <div className="Gel">
-        <img src="https://www.mattressclarity.com/wp-content/uploads/2017/07/KapokFiberImage.jpg" alt="pillow:>"/>
+        <img src="https://cdn.shopify.com/s/files/1/0011/1273/5803/products/Technogel-Anatomic_Pillow_No-Cover_1024x1024_clip-jcopy_1024x.jpg?v=1632441149" alt="pillow:>" className="pillowIMG"/>
         <li5>Gel</li5>
         <li6>Gel is made from materials originating in Silicone or Polyurethane. By nature Gel is neither a solid, nor a liquid, and it can provide the benefit of body temperature regulation. It is used increasingly in mattresses and pillows for its cooling properties as well as its ability to offer incredible pressure relief.</li6>
     </div>
     <div className="Latex">
-        <img src="https://www.mattressclarity.com/wp-content/uploads/2017/07/KapokFiberImage.jpg" alt="pillow:>"/>
+        <img src="https://m.media-amazon.com/images/I/613dB0bYsGL._AC_UF894,1000_QL80_.jpg" alt="pillow:>" className="pillowIMG"/>
         <li5>Latex</li5>
         <li6>Latex is a milky white liquid that is extracted from Heveabrasiliensis, also known as the ‘rubber tree’. This is obtained by tapping into the bark of the tree and collecting the resin. This milky white sap contains rubber particles dispersed in water, which is then converted into a formed pillow by adding fillers and other chemicals</li6>
     </div>
@@ -51,35 +51,35 @@ const PillowTalks = () => {
 
 
     </div>
-    <h2>Types of pillows:</h2>
+    <h2>Types of pillows</h2>
     <ul>
     <li9>
-        <b>Contour Pillows</b> Are shaped to accommodate the natural contours of the upper body. They are usually higher at one side and drop in the middle where the head rests. This provides for greater support under the neck as well as comfortable positioning of the cervical spine and for this reason they are also often known as neck pillows. When these pillows are used to help alleviate a medical condition such as jaw tension and pain, headaches, migraines, insomnia, shoulder pain, snoring, sleep apnoea and other conditions they may be referred to as orthopedic or therapeutic pillows.
+        <b1>Contour Pillows</b1> Are shaped to accommodate the natural contours of the upper body. They are usually higher at one side and drop in the middle where the head rests. This provides for greater support under the neck as well as comfortable positioning of the cervical spine and for this reason they are also often known as neck pillows. When these pillows are used to help alleviate a medical condition such as jaw tension and pain, headaches, migraines, insomnia, shoulder pain, snoring, sleep apnoea and other conditions they may be referred to as orthopedic or therapeutic pillows.
 
     </li9>
     <li9>
-        <b>Classic Pillows</b>Pillows without a contour. These may be good for sleeping on your back or stomach. Generally a contour pillow is preferred for a better, healthier sleep posture as a classic styled pillow will not provide the same level of support to your neck.
+        <b1>Classic Pillows</b1>Pillows without a contour. These may be good for sleeping on your back or stomach. Generally a contour pillow is preferred for a better, healthier sleep posture as a classic styled pillow will not provide the same level of support to your neck.
 
 
     </li9>
     <li9>
-        <b>Body Pillows</b>body pillows are a necessity for side sleepers. They provide proper support while you lay on your side. They provide pressure relief for the lower back, hips, and spine.
+        <b1>Body Pillows</b1>body pillows are a necessity for side sleepers. They provide proper support while you lay on your side. They provide pressure relief for the lower back, hips, and spine.
 They can also help relax the muscles that run along the spine. It has been shown that sleeping with a body pillow reduces tossing and turning while sleeping. Body pillows can also add another level of support during pregnancy.
 
     </li9>
     <li9>
-        <b>Pet Pillows </b>pet pillows come in the same materials that normal pillows are made with. They come in every size that a pet could require, no matter the size or shape. If you care for your pet, then their sleep should be one of your top priorities while providing them with a caring and loving home.
+        <b1>Pet Pillows </b1>pet pillows come in the same materials that normal pillows are made with. They come in every size that a pet could require, no matter the size or shape. If you care for your pet, then their sleep should be one of your top priorities while providing them with a caring and loving home.
         </li9>
         <li9>
-            <b>Decorative Pillows</b>These are pillows designed to add a preferred aesthetic to a room whether it be a bedroom, living room, or any room that you deem worthy of decoration. Decorative pillows aren’t known for their comfort, they are something to be looked at and maybe used as an extra level of support for the legs or the arms.
+            <b1>Decorative Pillows</b1>These are pillows designed to add a preferred aesthetic to a room whether it be a bedroom, living room, or any room that you deem worthy of decoration. Decorative pillows aren’t known for their comfort, they are something to be looked at and maybe used as an extra level of support for the legs or the arms.
 
         </li9>
         <li9>
-            <b>Neck Breakers</b> if you want to really get down to your ancient roots maybe our neck breakers would provide what you’re looking for. Our neck breakers are made from top quality bundles of sticks, freshly cut cinder blocks, and high quality sacks filled with dirt. If it can be shaped to even slightly resemble a pillow, we can make it for you.
+            <b1>Neck Breakers</b1> if you want to really get down to your ancient roots maybe our neck breakers would provide what you’re looking for. Our neck breakers are made from top quality bundles of sticks, freshly cut cinder blocks, and high quality sacks filled with dirt. If it can be shaped to even slightly resemble a pillow, we can make it for you.
 
         </li9>
         <li9>
-            <b>Outdoor and Travel Pillows</b> Whether youre traveling the trails or traveling the skies we make a pillow for all of your adventures. Outdoor and travel pillows can provide an easily packed blow up pillow, a very supportive horseshoe neck pillow for long flights, or anything in between.
+            <b1>Outdoor and Travel Pillows</b1> Whether youre traveling the trails or traveling the skies we make a pillow for all of your adventures. Outdoor and travel pillows can provide an easily packed blow up pillow, a very supportive horseshoe neck pillow for long flights, or anything in between.
 
 
         </li9>
